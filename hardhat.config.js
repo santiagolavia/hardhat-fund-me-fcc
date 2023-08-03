316604291d03333b95996d356adb51f99c198676
santiagolavia <laviasantiago@outlook.com>
2023-08-02 22:15:26 -0300

    recovery finished

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,48 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-gas-reporter");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+      hardhat: {
+          chainId: 31337,
+          // gasPrice: 130000000000,
+      },
+      sepolia: {
+          url: SEPOLIA_RPC_URL,
+          accounts: [PRIVATE_KEY],
+          chainId: 11155111,
+          blockConfirmations: 6,
+      },
+  },
+  solidity: {
+      compilers: [
+          {
+              version: "0.8.7",
+          },
+          {
+              version: "0.6.6",
+          },
+      ],
+  },
+  etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+      // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+},
+namedAccounts: {
+    deployer: {
+        default: 0, // here this will by default take the first account as deployer
+        1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+},
 };
