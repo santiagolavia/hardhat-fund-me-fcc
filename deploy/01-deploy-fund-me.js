9694246b2848f62676639585f907222f8c5c37c9
santiagolavia <laviasantiago@outlook.com>
2023-10-12 17:31:09 -0300

    corrientes

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,7 +1,4 @@
-const {
-    networkConfig,
-    developmentChains,
-} = require("../helper-hardhat-config.js")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { network } = require("hardhat")
 const { verify } = require("../utils/verify")
 
---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,24 @@
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+const { assert } = require("chai")
+
+// Only run on testnets
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Fund Me", async function () {
+          let fundMe
+          let deployer
+          const sendValue = ethers.parseEther("0.1")
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+          it("allows people to fund and withdraw", async function () {
+              await fundMe.fund({ value: sendValue })
+              await fundMe.withdraw()
+              const endingBalance = await ethers.provider.getBalance(
+                  await fundMe.address,
+              )
+              assert.equal(endingBalance.toString(), "0")
+          })
+      })
